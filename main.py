ee78eb2d39c968dcb121f4e7cf5da2ec2042b9e3
Gabriel Gonçalves <gabrielraeder@outlook.com.br>
2024-06-03 17:51:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,76 @@ from pagbank_crawler import run_pagbank_crawler
 from yuno_crawler import run_yuno_scraper
 from pagbank_get_all_text import run_pagbank_text_reader
 from yuno_get_all_text import run_yuno_text_reader
+from time import sleep
+
+
+def password_check():
+    password = input("Enter password: ")
+    is_correct = password == "123456"
+    tries = 3
+    while not is_correct:
+        sleep(0.5)
+        password = input(f"Wrong password... Try again({tries}): ")
+        is_correct = password == "123456"
+        tries -= 1
+        if tries == 0:
+            break
+    return is_correct
+
+
+def invalid_choice():
+    print("------------------------------------------")
+    print("Invalid choice. Please enter 1, 2, or 3.")
+    sleep(1)
+
+
+def yuno_choices():
+    while True:
+        print("------------------------------------------")
+        print("Do you want to:")
+        print("------------------------------------------")
+        print("1. Run crawler")
+        print("2. Update translated content")
+        print("3. Go back")
+        print("------------------------------------------")
+
+        yuno_choice = input("Enter the number of your choice: ")
+        sleep(0.5)
+
+        if yuno_choice == "1":
+            run_yuno_scraper()
+        elif yuno_choice == "2":
+            correct_password = password_check()
+            if correct_password:
+                run_yuno_text_reader()
+            else:
+                break
+        elif yuno_choice == "3":
+            break
+        else:
+            invalid_choice()
+
+
+def pagbank_choices():
+    while True:
+        print("Do you want to:")
+        print("1. Run crawler")
+        print("2. Update translated content")
+        print("3. Go back")
+        print("------------------------------------------")
+
+        pagbank_choice = input("Enter the number of your choice: ")
+
+        if pagbank_choice == "1":
+            run_pagbank_crawler()
+        elif pagbank_choice == "2":
+            correct_password = password_check()
+            if correct_password:
+                run_pagbank_text_reader()
+        elif pagbank_choice == "3":
+            break
+        else:
+            invalid_choice()
 
 
 def main():
@@ -15,54 +85,20 @@ def main():
         print("------------------------------------------")
 
         service_choice = input("Enter the number of your choice: ")
+        sleep(0.5)
 
         if service_choice == "1":
-            while True:
-                print("------------------------------------------")
-                print("Do you want to:")
-                print("------------------------------------------")
-                print("1. Run crawler")
-                print("2. Update translated content")
-                print("3. Go back")
-                print("------------------------------------------")
+            yuno_choices()
 
-                yuno_choice = input("Enter the number of your choice: ")
-
-                if yuno_choice == "1":
-                    run_yuno_scraper()
-                elif yuno_choice == "2":
-                    run_yuno_text_reader()
-                elif yuno_choice == "3":
-                    break
-                else:
-                    print("------------------------------------------")
-                    print("Invalid choice. Please enter 1, 2, or 3.")
         elif service_choice == "2":
-            while True:
-                print("Do you want to:")
-                print("1. Run crawler")
-                print("2. Update translated content")
-                print("3. Go back")
-                print("------------------------------------------")
+            pagbank_choices()
 
-                pagbank_choice = input("Enter the number of your choice: ")
-
-                if pagbank_choice == "1":
-                    run_pagbank_crawler()
-                elif pagbank_choice == "2":
-                    run_pagbank_text_reader()
-                elif pagbank_choice == "3":
-                    break
-                else:
-                    print("------------------------------------------")
-                    print("Invalid choice. Please enter 1, 2, or 3.")
-                    print("------------------------------------------")
         elif service_choice == "3":
             print("Exiting...")
+            sleep(2)
             break
         else:
-            print("------------------------------------------")
-            print("Invalid choice. Please enter 1, 2, or 3.")
+            invalid_choice()
 
 
 if __name__ == "__main__":
